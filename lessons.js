dcca7c36b501cd0cfda45bb86d713a3c105c8cc8
Bazaid Khan <bazaid-k@hotmail.com>
2021-02-12 21:39:24 +0300

    init

---
+++ lessons.js
@@ -0,0 +1,83 @@
+let products = [
+    {
+      "id": 1001,
+      "title": "Math",
+      "description": "London",
+      "price": 100,
+      "availableInventory": 5,
+      "image": "math.jpg"
+    },
+    {
+      "id": 1002,
+      "title": "Physics",
+      "description": "Oxford",
+      "price": 100,
+      "availableInventory": 5,
+      "image": "physics.png"
+    },
+    {
+      "id": 1003,
+      "title": "Biology",
+      "description": "London",
+      "price": 90,
+      "availableInventory": 5,
+      "image": "Biology.jpg"
+    },
+    {
+      "id": 1004,
+      "title": "Chemistry",
+      "description": "Oxford",
+      "price": 70,
+      "availableInventory": 5,
+      "image": "Chemistry.jpg"
+    },
+    {
+      "id": 1005,
+      "title": "English",
+      "description": "Bristol",
+      "price": 100,
+      "availableInventory": 5,
+      "image": "English.jpeg"
+    },
+    {
+      "id": 1006,
+      "title": "Business Studies",
+      "description": "York",
+      "price": 90,
+      "availableInventory": 5,
+      "image": "Business.png"
+    },
+    {
+      "id": 1007,
+      "title": "IT",
+      "description": "London",
+      "price": 100,
+      "availableInventory": 5,
+      "image": "It.png"
+    },
+    {
+      "id": 1008,
+      "title": "Psychology",
+      "description": "Oxford",
+      "price": 90,
+      "availableInventory": 5,
+      "image": "psychology.png"
+    },
+    {
+      "id": 1009,
+      "title": "History",
+      "description": "Bristol",
+      "price": 60,
+      "availableInventory": 5,
+      "image": "history.jpg"
+    },
+    {
+      "id": 1010,
+      "title": "Arts",
+      "description": "Oxford",
+      "price": 70,
+      "availableInventory": 5,
+      "image": "arts.jpg"
+    }
+    
+]
